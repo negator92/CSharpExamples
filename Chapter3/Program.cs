834013a98ab33ce61a0cc8215571a2ae6901f6ff
Vincent Neparidze <negator92@gmail.com>
2018-08-15 15:16:54 +0300

    Adding missed projects

--- Chapter3/Program.cs
+++ Chapter3/Program.cs
@@ -7,7 +7,7 @@ namespace Chapter3
         static void Main()
         {
             var arr1 = new[] {1, 2, 3};
-            Console.WriteLine($"Type of array is {arr1.GetType()}");
+            Console.WriteLine($"Type of array is {arr1.GetType()}");czx
         }
     }
 }
